a0d50b77ff745813ade68449c3691bfeeae7c12a
esack7 <isaacheist@gmail.com>
2017-10-13 14:02:12 -0700

    set up frontend, still need to configure for deployment

---
+++ src/component/home-container/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HomeContainer extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1> Welcome!  You need to login.</h1>
+      </div>
+    );
+  }
+}
+
+export default HomeContainer;
---
+++ src/lib/redux-reporter.js
@@ -0,0 +1,14 @@
+let reporter = store => next => action => {
+  console.log('__ACTION__', action);
+
+  try {
+    let result = next(action);
+    console.log('__STATE__', store.getState());
+  } catch (e) {
+    e.action = action;
+    console.error('__ERROR__', e);
+    return e;
+  }
+};
+
+export default reporter;
